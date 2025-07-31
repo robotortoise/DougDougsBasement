32c97c12c0b8b8939b885f164a8a057f9ee40900
Jordan Brown <robotortoise@gmail.com>
2025-07-31 14:48:12 -0700

    doug doug commit
    a

---
+++ Assets/UserInterface/NaninovelUIPrefabs/HelloNametag/HelloMyNameIs_InputValidator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[CreateAssetMenu(fileName = "HelloMyNameIsInputValidator", menuName = "Input Field Validator (HelloMyNameIs)")]
+public class CustomValidator : TMPro.TMP_InputValidator
+{
+    public override char Validate(ref string text, ref int pos, char ch)
+    {
+	    if (text.Length >= 11) return '\0';
+	    if (ch == '<') return '\0';
+	    if (ch == '>') return '\0'; 
+	    text = text.Insert(pos, ch.ToString());
+	    pos++;
+	    return ch;
+    }
+}
